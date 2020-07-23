d8180cb1254783508fb5c01c93f810fac5507c8a
Maycie Morris <maycie@MacBook-Pro.local>
2020-07-23 11:13:26 -0700

    Task 1

--- index.js
+++ index.js
@@ -41,6 +41,23 @@ class Airplane {
 */
 
 class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
+
+  }
+  eat(food) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(food);
+    }
+  }
+  poop() {
+    this.stomach = [];
+  }
+  toString() {
+    return `${this.name} and ${this.age}`
+  }
 
 }
 
